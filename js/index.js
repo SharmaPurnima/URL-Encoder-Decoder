cd5fd75a0d25203d1f2c3896ebcadfdbabebcd25
SharmaPurnima <purnimasharmarke@gmail.com>
2018-06-10 16:33:20 +0530

    URL Encoder Decoder

--- js/index.js
+++ js/index.js
@@ -3,6 +3,11 @@ $(document).ready(function() {
 $("#firstButton").on("click", function() {
     var inputURL = $("#urlvalue").val();
     var EncodedURL =encodeURI(inputURL);
-    $("#EncodedURL").text(EncodedURL);
+    $("#OutputURL").text(EncodedURL);
+  }); 
+  $("#secondButton").on("click", function() {
+    var inputURL = $("#urlvalue").val();
+    var DecodedURL =decodeURI(inputURL);
+    $("#OutputURL").text(DecodedURL);
   }); 
 });
